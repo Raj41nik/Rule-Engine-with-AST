e659fcc2ae1a057c33423ff0fcf2bce5257f45ca
Raj41nik <raj40rockstanik@gmail.com>
2024-10-22 14:19:33 +0530

    vercal config1

--- app.py
+++ app.py
@@ -21,7 +21,4 @@ def index():
 def static_files(path):
     return send_from_directory('static', path)
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Create the database tables
-    app.run(debug=True)
+
